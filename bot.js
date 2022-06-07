f666bf36d15ab9d1285666edf72357e370582d9d
Memo Khoury <majd.khoury93@gmail.com>
2022-06-07 00:12:35 -0400

    graphql

--- bot.js
+++ bot.js
@@ -1,6 +1,25 @@
 var Discord = require("discord.io");
 var logger = require("winston");
 var auth = require("./auth.json");
+var gql = require("graphql-request");
+
+// graphql client
+const client = new gql.GraphQLClient(
+  "https://thegraph.com/hosted-service/subgraph/teller-protocol/teller-v2"
+);
+
+const returnBidDetails = async (address) => {
+  const query = gql.gql`
+    {
+      bids(where: { borrowerAddress: ${address} }) {
+        borrowerAddress
+      }
+    }
+  `;
+
+  const data = await client.request(query);
+  return data;
+};
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -17,22 +36,17 @@ bot.on("ready", function (evt) {
   logger.info(bot.username + " - (" + bot.id + ")");
 });
 
-const checkIfAddress = (address) => {
-  const result = /^hello/.test(address);
-  return result;
-};
-
 bot.on(
   "message",
   async function (user, userID, channelID, message, evt, interaction) {
-    console.log(message);
     if (message.substring(0, 1) == "!") {
       var args = message.substring(1).split(" ");
-
       var cmd = args[0];
-
       args = args.splice(1);
+
       if (/0x[a-fA-F0-9]{40}/g.test(cmd)) {
+        const details = await returnBidDetails(cmd);
+        console.log(details);
         bot.sendMessage({
           to: channelID,
           message: "Yo that looks like a correct Ethereum address!",
